6e2e629f9512e96ba7a5b0db3e545aada8f3a71f
crumb <yanguzovgr@gmail.com>
2023-11-07 07:12:21 +0500

    создание классов. yanguzov

---
+++ Classes/Common/CheckRegex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_yanguzov.Classes.Common
+{
+    internal class CheckRegex
+    {
+    }
+}
---
+++ Classes/Passport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_yanguzov.Classes
+{
+    public class Passport
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Issued { get; set; }
+        public string DateOfIssued { get; set; }
+        public string DepartmentCode { get; set; }
+        public string SeriesAndNumber { get; set; }
+        public string DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set;}
+
+    }
+}
